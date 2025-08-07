ea82db91177b3ce652c0aafb4471009bc3374324
Marcio Samogin <marcio@unicamp.br>
2025-08-07 15:53:06 -0300

    Update create-users.sh

--- create-users.sh
+++ create-users.sh
@@ -6,16 +6,17 @@ echo "Configurar acesso usuario ludgero"
 /root/shell/./create-user-rocky-ludgero.sh
 ##########################################
 echo "Configurar acesso usuario marcio"
-/root/shell/./create-user-marcio.sh
+/root/shell/./create-user-rocky-marcio.sh
 ##########################################
 echo "Configurar acesso usuario keite"
-/root/shell/./create-user-keite.sh
+/root/shell/./create-user-rocky-keite.sh
 ##########################################
 echo "Configurar acesso usuario otoniel"
-/root/shell/./create-user-otoniel.sh
+/root/shell/./create-user-rocky-otoniel.sh
 ##########################################
 echo "Configurar acesso usuario danfeijo"
-/root/shell/./create-user-danfeijo.sh
+/root/shell/./create-user-rocky-danfeijo.sh
 ##########################################
 echo "Copiar as chaves privadas para a maquina Linux auxiliar"
 scp *@$hostname marcio@143.106.108.5:/tmp
+rm -rf *@$hostname
