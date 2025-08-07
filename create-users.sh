4f05d81fab4119885144f84cabcd4eade7bc07ee
Marcio Samogin <marcio@unicamp.br>
2025-08-07 16:05:36 -0300

    Update create-users.sh

--- create-users.sh
+++ create-users.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 # Capturar o nome da maquina
 hostname=`hostname`
-# Instalar o git
-yum install git -y
-# Criar diretorio shell e entrar nele
-mkdir /root/shell && cd /root/shell
-# Baixar shell do Github
-git clone https://github.com/marsmogin/linux/rocky
-# Mover arquivos para diretorio shell
-mv /rocky/*.sh .
-# Adicionar permissao de execuçao aos aqruivos
-chmod +x *.sh
 ##########################################
 ##########################################
 echo "Configurar acesso usuario ludgero"
