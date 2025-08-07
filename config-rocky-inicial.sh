8b2e11daed0744793cf4e8c2c4dc8b3242606ebc
Marcio Samogin <marcio@unicamp.br>
2025-08-07 16:17:18 -0300

    Update config-rocky-inicial.sh

--- config-rocky-inicial.sh
+++ config-rocky-inicial.sh
@@ -1,11 +1,13 @@
-#!/bin/bash
+#!/bin/bash -xv
 # Instalar o git
 yum install git -y
 # Criar diretorio shell e entrar nele
 mkdir /root/shell && cd /root/shell
 # Baixar shell do Github
-git clone https://github.com/marsmogin/linux/rocky
+git clone https://github.com/marsamogin/rocky
 # Mover arquivos para diretorio shell
-mv /rocky/*.sh .
+mv /root/shell/rocky/*.sh .
 # Adicionar permissao de execuçao aos aqruivos
 chmod +x *.sh
+# Remove diretorio baixado
+rm -rf /root/shell/rocky
